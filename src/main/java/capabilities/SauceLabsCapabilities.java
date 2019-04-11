f5c802a675ae173d6f2d873c2e5ad2c7dcbe1ff2
Tanu Mukherjee <tanu@saavn.com>
2019-04-11 13:49:46 +0530

    update

--- src/main/java/capabilities/SauceLabsCapabilities.java
+++ src/main/java/capabilities/SauceLabsCapabilities.java
@@ -21,23 +21,19 @@ public class SauceLabsCapabilities extends DriverManager {
     @Override
     public AndroidDriver<AndroidElement>  createDriver() throws Exception {
     						//new DesiredCapabilities();
-		DesiredCapabilities capabilities = DesiredCapabilities.iphone();
-		capabilities.setCapability("app", "sauce-storage:Saavn.zip");		
+		DesiredCapabilities capabilities = DesiredCapabilities.iphone();	
 		capabilities.setCapability("appiumVersion", "1.8.1");
 		capabilities.setCapability("deviceName","iPhone X Simulator");
 		capabilities.setCapability("deviceOrientation", "portrait");
 		capabilities.setCapability("platformVersion","11.3");
-		capabilities.setCapability("platformName", "iOS");
 		capabilities.setCapability("browserName", "");
-		capabilities.setCapability("app","sauce-storage:Saavn.zip");
 		
 		
     	System.out.println("SCL 4");
 	//	driver = DriverConnection.getDriverInstance("sauceLabs", capabilities).getDriver();
     	driver = new AndroidDriver<AndroidElement>(
-				new URL("http://testsaavnandroid:0208cf09-abcf-408a-9f03-c7a1efc74d3b@ondemand.saucelabs.com:80/wd/hub"),
+				new URL("username"),
 				capabilities);
-    	System.out.println("SCL DRIVER");
 		return driver;
 		
 	}
